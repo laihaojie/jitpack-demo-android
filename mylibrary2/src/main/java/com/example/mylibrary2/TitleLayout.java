af110e52c978b5e36fc745f29432c11b34b21e47
laihaojie <53169820+laihaojie@users.noreply.github.com>
2021-06-12 11:06:44 +0800

    ,

---
+++ mylibrary2/src/main/java/com/example/mylibrary2/TitleLayout.java
@@ -0,0 +1,51 @@
+package com.example.mylibrary2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.example.mylibrary2.R;
+
+public class TitleLayout extends LinearLayout {
+
+    private Button btn1;
+    private Button btn2;
+    public TitleLayout(Context context) {
+        super(context);
+    }
+
+    public TitleLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        btn1 = findViewById(R.id.Title_back);
+        btn2 = findViewById(R.id.Title_edit);
+        btn1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        btn2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"wowoowowowow",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public TitleLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TitleLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
